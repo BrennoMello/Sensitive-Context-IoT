de1b97dd8600516f890388715d72a688bee2915e
Brenno <brennodemello@hotmail.com>
2017-08-08 19:59:54 -0300

    Testes Iniciais

--- src/main/java/br/com/ufba/sensitivecontextiot/inference/Main.java
+++ src/main/java/br/com/ufba/sensitivecontextiot/inference/Main.java
@@ -5,10 +5,34 @@
  */
 package br.com.ufba.sensitivecontextiot.inference;
 
+
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDFS;
+
 /**
  *
  * @author brenno
  */
 public class Main {
-    
+    public static void main(String [] args){
+        String NS = "urn:x-hp-jena:eg/";
+
+        // Build a trivial example data set
+        Model rdfsExample = ModelFactory.createDefaultModel();
+        Property p = rdfsExample.createProperty(NS, "p");
+        Property q = rdfsExample.createProperty(NS, "q");
+        rdfsExample.add(p, RDFS.subPropertyOf, q);
+        rdfsExample.createResource(NS+"a").addProperty(p, "foo");
+        
+        InfModel inf = ModelFactory.createRDFSModel(rdfsExample);
+        
+        Resource a = inf.getResource(NS+"a");
+        System.out.println("Statement: " + a.getProperty(q));
+        
+        
+    }
 }
